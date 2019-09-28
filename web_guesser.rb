631c6baec422dbb688159f4570a5af5ce100d542
Ashley Kung <ashley.berg@gmail.com>
2019-09-28 13:51:22 -0400

    Add guess form and display hints

--- web_guesser.rb
+++ web_guesser.rb
@@ -4,5 +4,22 @@ require 'sinatra/reloader'
 number = rand(100)
 
 get '/' do
-  erb :index, :locals => { :number => number }
+  guess = params["guess"]
+  message = check_guess(guess, number)
+  erb :index, :locals => { :number => number, :message => message }
+end
+
+def check_guess(guess, number)
+  case
+  when guess.to_i > number+5
+    "Way too high!"
+  when guess.to_i < number-5
+    "Way too low!"
+  when guess.to_i > number
+    "Too high!"
+  when guess.to_i < number
+    "Too low!"
+  else
+    "You got it right!"
+  end
 end
